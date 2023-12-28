223f01cd7dfd74d89249c62773b81ddf749d636b
kimimachi jou <kimimachi jou@192.168.10.103>
2023-12-28 21:53:08 +0900

    課題3の提出

---
+++ HelloWorld_SekiokaJouichirou/src/sample/SelfIntroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// 氏名
+		String name = "関岡 丈一郎";
+		// 年齢
+		int age = 28;
+		// 趣味
+		String hobby = "TRPG";
+		
+		
+		/*
+		 *  自己紹介文に出力する処理
+		 */
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}
